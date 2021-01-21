51cc36ec196b8e0c6486b5b06dcd784028cf83fc
Yin <yinnnn17@gmail.com>
2021-01-20 16:09:53 -0800

    added search form

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// Routes
+// ROUTES
+// example URL "http://localhost:3000/?term=hey"
 app.get('/', (req, res) => {
-  // set the url of the gif
-  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-  // render the hello-gif view, passing the gifUrl into the view to be displayed
-  res.render('hello-gif', { gifUrl })
+  console.log(req.query) // => "{ term: hey" }
+
+  res.render('home')
 })
 app.get('/greetings/:name', (req, res) => {
   // grab the name from the path provided
